904cfbdcd2e47f148bcb0085191ebd7e7c2a9d9b
jmalvare <jmalvare@ucsd.edu>
2021-08-30 18:02:14 -0700

    created post and get, changed / format in docker for my config

--- server/server.js
+++ server/server.js
@@ -1,9 +1,34 @@
 const express = require( 'express' );
+const addressController = require('./controllers/address/index');
+
 
 const app = express();
+app.use(express.json());
 
-app.get( '/', ( req, res ) => {
-  res.send({ greeting: 'Hello world!' });
+// grab one address
+app.get( '/address-book/:id', async ( req, res ) => {
+  let addrId = req.params.id;
+  try {
+    const getAddress = await addressController.get(addrId);
+    res.status(200).json(getAddress);//still sends even if id not found
+
+  } catch (error) {
+    res.send(error);
+  }
 });
+
+// add an address
+app.post( '/address-book', async (req, res) => {
+  let address = { line1: req.body.line1, city: req.body.city, state: req.body.state, zip: req.body.zip};
+  try {
+    let addrId = await addressController.add(address); //returns ID
+    const addrData = await addressController.get( addrId);
+    res.send({'new id':addrId})
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//update an address
 
 app.listen( process.env.PORT );
